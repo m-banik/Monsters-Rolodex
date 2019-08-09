bba425cb1f62c5b9700ef1413cc61bff77c8160b
Marcin Banik <mrn.banik@gmail.com>
2019-08-10 00:40:35 +0200

    Updates

---
+++ precache-manifest.56fd68c539b6e7f44c3fe0298f00639d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2c5eddb467cef16833b6edcd5dcd1f9f",
+    "url": "/Monsters-Rolodex/index.html"
+  },
+  {
+    "revision": "6563eda196b05baada6e",
+    "url": "/Monsters-Rolodex/static/css/main.1e5fd406.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/Monsters-Rolodex/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "6563eda196b05baada6e",
+    "url": "/Monsters-Rolodex/static/js/main.6a38360f.chunk.js"
+  },
+  {
+    "revision": "3c42c109e430c626051d",
+    "url": "/Monsters-Rolodex/static/js/runtime~main.2f1dcf62.js"
+  }
+]);
